11483bf4440b6cf91330e3afc5be20454347b4d7
UNCCarlosJimenez <carlos_jimenez@unicomer.com>
2017-02-15 16:16:34 -0600

    JBoss sin funcionar.... problema de classpath

--- harvester-excel/src/main/java/com/unicomer/oer/harvester/reader/JBossRemoteReader.java
+++ harvester-excel/src/main/java/com/unicomer/oer/harvester/reader/JBossRemoteReader.java
@@ -4,24 +4,27 @@
 package com.unicomer.oer.harvester.reader;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import javax.management.MBeanServerConnection;
-import javax.management.ObjectName;
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.sasl.RealmCallback;
 
-import com.oracle.artifact.ArtifactAlgorithm;
-import com.oracle.oer.sync.framework.MetadataIntrospectionException;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.as.controller.client.helpers.ClientConstants;
+import org.jboss.as.controller.client.helpers.Operations;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.Property;
+
 import com.oracle.oer.sync.framework.MetadataLogger;
 import com.oracle.oer.sync.framework.MetadataManager;
 import com.oracle.oer.sync.framework.MetadataReader;
@@ -36,178 +39,223 @@ public class JBossRemoteReader implements MetadataReader {
 	private static MetadataLogger logger = MetadataManager.getLogger(JBossRemoteReader.class);
 	
 	public List<Set<Entity>> read() throws Exception {
-		List<Set<Entity>> list = new ArrayList<Set<Entity>>();
+		String hostname = "oraposdms.unicomer.com";
+		String username = "administrator";
+		String password = "password";
+		int port = 9999;
 		
+		List<Set<Entity>> result = new ArrayList<Set<Entity>>();
 		HashMap<String, Entity> serverMap = new HashMap<String, Entity>();
 		HashMap<String, Entity> applicationMap = new HashMap<String, Entity>();
-		HashMap<String, Entity> libraryMap = new HashMap<String, Entity>();
+		HashMap<String, Entity> componentMap = new HashMap<String, Entity>();
 		
 		try{
-			String urlString = "http://uinhsap1wldev.datacenter.milady.local:7091/";
-			String username = "weblogic";
-			String password = "Un1c0m3r";
-			final ObjectName service;
-			String jndiPath = "/jndi/weblogic.management.mbeanservers.domainruntime";// "/jndi/weblogic.management.mbeanservers.runtime";
-			UnicomerEntity serverEntity = null;
-			UnicomerEntity applicationEntity = null;
-			UnicomerEntity libraryEntity = null;
-						
-			JMXConnector jmxConn = initConnection(urlString, username, password, jndiPath);
-			MBeanServerConnection mbconn = jmxConn.getMBeanServerConnection();
+			ModelControllerClient client = createClient(InetAddress.getByName(hostname), port, username, password, "ManagementRealm");
+			logger.info("Conexion establecida!");
 			
-			service = new ObjectName(
-					// "com.bea:Name=DomainRuntimeService,Type=*");
-					"com.bea:Name=DomainRuntimeService,Type=weblogic.management.mbeanservers.domainruntime.DomainRuntimeServiceMBean");
+            //Se obtienen generalidades del ambiente
+			ModelNode op = new ModelNode();
+			op = Operations.createOperation("read-resource");
+			op.get("profile").set("default");
+            op.get("include-runtime").set(true);
+            op.get("include-defaults").set(true);
+            ModelNode productInfo = client.execute(op);
+            
+            if(Operations.isSuccessfulOutcome(productInfo)){
+            	
+            	// Se obtienen todos las aplicaciones desplegadas en cada servidor
+            	op = new ModelNode();
+            	op = Operations.createOperation("read-children-resources");
+                op.get(ClientConstants.CHILD_TYPE).set(ClientConstants.SERVER_GROUP);
+                op.get("recursive").set(true);
+                op.get("include-runtime").set(true);
+                op.get("include-defaults").set(true);
+                ModelNode serverGroupResult = client.execute(op);
+                
+                if(Operations.isSuccessfulOutcome(serverGroupResult)){
+                	// Operations.readResult(result).require("summary")
+        			ModelNode serverGroups = serverGroupResult.get("result");
+        			Iterator<ModelNode> it =  serverGroups.asList().iterator();
+        			while(it.hasNext()){
+        				ModelNode serverNode = it.next();
+        				List<Property> properties = serverNode.asPropertyList();
+        				Property property = properties.get(0);
+        				String serverGroupName = property.getName();
+        				StringBuilder jvmDetails = new StringBuilder("");
+        				logger.info("Server Group: " + serverGroupName);
+        				//Obtener detalles de JVM
+        				Iterator<Property> jvm = property.getValue().get("jvm").asPropertyList().iterator();
+        				while(jvm.hasNext()){
+        					jvmDetails.append(jvm.next().getValue().asString()).append(System.getProperty("line.separator"));
+        				}
+        				Entity serverEntity = createServerEntity(serverGroupName, jvmDetails, productInfo);
+        				serverMap.put(serverGroupName, serverEntity);
+        				
+        				//Obtener detalle de despliegues
+        				Iterator<Property> deployments = property.getValue().get("deployment").asPropertyList().iterator();
+        				while(deployments.hasNext()){
+        					ModelNode deployment = deployments.next().getValue();
+        					if(deployment.get("enabled").asBoolean()){
+        						String deploymentName = deployment.get("runtime-name").asString();
+        						logger.info("    " + deploymentName);
+        						Entity applicationEntity = createApplicationEntity(deploymentName, serverEntity);
+                				serverMap.put(deploymentName, applicationEntity);
+        					}
+        				}
+        				logger.info("");
+        			}
+        		}else{
+        			logger.error("La consulta a JBoss ha fallado: " + serverGroupResult.get("result").asString());
+        		}                
+            }
 			
-			ObjectName[] serverRuntimes = (ObjectName[]) mbconn.getAttribute(service, "ServerRuntimes");
-			logger.info("Se obtiene " + serverRuntimes.length
-					+ " servidores manejados del dominio. Para cada servidor manejado se obtienen aplicaciones y datasources");
-						
-			for (ObjectName serverRuntime:serverRuntimes) {
-				String serverName = (String) mbconn.getAttribute(serverRuntime, "Name");
-				logger.info("> " + serverName);
-				serverEntity = new UnicomerEntity("Environment", serverName, serverName, serverName, "1.0.0", ArtifactAlgorithm.DEFAULT);
-				serverMap.put(serverName, serverEntity);
-				
-//				logger.info("  Aplicaciones desplegadas:");
-				ObjectName[] applicationRuntimes = (ObjectName[]) mbconn.getAttribute(serverRuntime, "ApplicationRuntimes");
-				for (ObjectName applicationRuntime:applicationRuntimes) {
-					String path ="";
-					String applicationName = (String) mbconn.getAttribute(applicationRuntime, "Name");
-					if (applicationMap.containsKey(applicationName)){
-						//Se actualiza la referencia
-						applicationEntity = (UnicomerEntity) applicationMap.get(applicationName);
-						applicationMap.remove(applicationName);
-					}else{
-						// Se crea la referencia
-						applicationEntity = new UnicomerEntity("Application", applicationName, applicationName, applicationName, "1.0.0", ArtifactAlgorithm.DEFAULT);
-						applicationEntity.addCategorization("AssetFunction", "Creacion manual");
-					}
-					applicationEntity.addRelationship(serverEntity, "Deployment", false);
-					applicationMap.put(applicationName, applicationEntity);
-					logger.info("    " + applicationName);
-					
-//					ObjectName[] componentRuntimes = (ObjectName[]) mbconn.getAttribute(applicationRuntime, "ComponentRuntimes");
-//					
-//					for (ObjectName componentRuntime:componentRuntimes) {
-//						String componentType = (String) mbconn.getAttribute(componentRuntime, "Type");
-////						logger.info("        " + (String) mbconn.getAttribute(componentRuntime, "Name")
-////								+ " de tipo " + componentType);
-//						
-//						if (componentType.toString().equals("WebAppComponentRuntime")) {
-//							ObjectName[] servletRTs = (ObjectName[]) mbconn.getAttribute(componentRuntime,"Servlets");
-//							int servletLength = (int) servletRTs.length;
-//							for (int z = 0; z < servletLength; z++) {
-//								path = (String) mbconn.getAttribute(servletRTs[z], "ContextPath");
-////								logger.info(
-////										" Servlet name: " + (String) mbconn.getAttribute(servletRTs[z], "Name"));
-////								logger.info(" Servlet context path: "
-////										+ (String) mbconn.getAttribute(servletRTs[z], "ContextPath"));
-////								logger.info(" Invocation Total Count : "
-////										+ (Object) mbconn.getAttribute(servletRTs[z], "InvocationTotalCount"));
-//							}
-//						}
-//					}					
-				}
-				
-				logger.info("Librerias desplegadas:");
-				ObjectName[] libraryRuntimes = (ObjectName[]) mbconn.getAttribute(serverRuntime, "LibraryRuntimes");
-				
-				for (ObjectName library : libraryRuntimes) {
-					String libraryName = (String) mbconn.getAttribute(library, "LibraryName");
-					String libraryVersion = (String) mbconn.getAttribute(library, "SpecificationVersion");
-					
-					if (libraryMap.containsKey(libraryName)){
-						//Se actualiza la referencia
-						libraryEntity = (UnicomerEntity) libraryMap.get(libraryName);
-						libraryMap.remove(libraryName);
-					}else{
-						// Se crea la referencia
-						libraryEntity = new UnicomerEntity("Application", libraryName, libraryName, libraryName, libraryVersion, ArtifactAlgorithm.DEFAULT);
-						libraryEntity.addCategorization("AssetFunction", "Creacion manual");
-					}
-					libraryEntity.addRelationship(serverEntity, "Deployment", false);
-					libraryMap.put(libraryName, libraryEntity);
-					logger.info("    " + libraryName);
-					
-					ObjectName[] referencingRuntimes = (ObjectName[]) mbconn.getAttribute(library, "ReferencingRuntimes");
-					for (ObjectName reference : referencingRuntimes) {
-						String referenceName = (String) mbconn.getAttribute(reference, "Name");
-						logger.info("        " + referenceName);
-						
-//						entity = new ArtifactEntity("Artifact: Jar", "", (String) mbconn.getAttribute(reference, "Name"), "", (String) mbconn.getAttribute(library, "Name"), "1.0", ArtifactAlgorithm.DEFAULT);
-						
-					}
-				}
-			}
-			
-			// Set<ObjectInstance> serverRT = mbconn.queryMBeans(null, null);
-			// logger.info("got server runtimes: "+serverRT.size());
-			// int length = (int) serverRT.size();
-			//
-			// Iterator<ObjectInstance> it = serverRT.iterator();
-			// while(it.hasNext()){
-			// ObjectInstance objectInstance = it.next();
-			// logger.info(objectInstance.toString());
-			// logger.info(objectInstance.getObjectName().getKeyPropertyListString());
-			// logger.info(objectInstance.getObjectName().getDomain());
-			// logger.info("");
-			// }
-			jmxConn.close();
+            client.close();
 		}catch(Exception e){
 			System.err.println("Ha ocurrido un problema: " + e.getMessage());
 			e.printStackTrace();
 		}
-		list.add(new HashSet<Entity>(serverMap.values()));
-		list.add(new HashSet<Entity>(applicationMap.values()));
-		list.add(new HashSet<Entity>(libraryMap.values()));
+		result.add(new HashSet<Entity>(serverMap.values()));
+		result.add(new HashSet<Entity>(applicationMap.values()));
+		result.add(new HashSet<Entity>(componentMap.values()));
 		
-		return list;
+		return result;
 	}
 	
-	private static JMXConnector initConnection(String urlString, String username, String password, String jndiPath)
-			throws MetadataIntrospectionException {
-		String lProtocol = "t3";
-		if ((urlString.toLowerCase().startsWith("https")) || (urlString.toLowerCase().startsWith("t3s"))) {
-			lProtocol = "t3s";
-		}
-		try {
-			URI uri = new URI(urlString);
-			if (uri.getHost() == null) {
-				uri = new URI(new StringBuilder().append(lProtocol).append("://").append(urlString).toString());
-			}
-			if ((uri.getHost() == null) || (uri.getPort() < 0)) {
-				throw new MetadataIntrospectionException(
-						"Invalid argument: -remote_url.  Expected a URL in the form of <host>:<port>");
-			}
-
-			JMXServiceURL serviceURL = new JMXServiceURL(lProtocol, uri.getHost(), uri.getPort(), jndiPath);
-
-			logger.info(new StringBuilder().append("Connecting to: ").append(serviceURL).toString());
-			Hashtable h = new Hashtable();
-			h.put("java.naming.security.principal", username);
-			h.put("java.naming.security.credentials", password);
-			h.put("jmx.remote.protocol.provider.pkgs", "weblogic.management.remote");
-
-			JMXConnector connector = JMXConnectorFactory.newJMXConnector(serviceURL, h);
-			connector.connect();
-			return connector;
-		} catch (URISyntaxException e) {
-			throw new MetadataIntrospectionException(
-					new StringBuilder().append("Unable to parse remote server URL: ").append(urlString).toString(), e);
-		} catch (MalformedURLException e) {
-			throw new MetadataIntrospectionException(
-					new StringBuilder().append("Unable to parse remote server URL: ").append(urlString).toString(), e);
-		} catch (IOException e) {
-			throw new MetadataIntrospectionException(new StringBuilder().append("Unable to connect to remote server: ")
-					.append(urlString).append(".  Please make sure the remote URL, username, and password are correct.")
-					.toString(), e);
-		}
-	}
-
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	private Entity createServerEntity (String serverName, StringBuilder jvmDetails, ModelNode productInfo){
+		Entity entity = new UnicomerEntity();
+		StringBuilder description = new StringBuilder("");
+		String productVersion = Operations.readResult(productInfo).require("product-version").asString();
+    	String releaseCodeName = Operations.readResult(productInfo).require("release-codename").asString();
+    	String releaseVersion = Operations.readResult(productInfo).require("release-version").asString();
+    	String productName = Operations.readResult(productInfo).require("product-name").asString();
+		
+    	description.append("Product Name: " + productName).append(System.getProperty("line.separator"));
+    	description.append("Product Version: " + productVersion).append(System.getProperty("line.separator"));
+    	description.append("Release Code Name: " + releaseCodeName).append(System.getProperty("line.separator"));
+    	description.append("Release Version: " + releaseVersion).append(System.getProperty("line.separator"));
+    	description.append(jvmDetails);
+    	
+		entity.setAssetType("Environment");
+		entity.setName(serverName);
+		entity.setVersion(releaseVersion);
+		entity.setDescription(description.toString());
+			
+		return entity;
+	}
+	
+	private Entity createApplicationEntity (String deploymentName, Entity server){
+		Entity entity = new UnicomerEntity();
+		String module = "";
+		String name = "";
+		String version = "";
+		String extension = "";
+		
+		try{
+			int i = deploymentName.lastIndexOf('.');
+			if (i >= 0) {
+			    extension = deploymentName.substring(i+1);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Exception occured while parsing extension from deployment " + deploymentName + ". " + e.getMessage());
+			extension = "";
+		}
+		
+		try{
+			version = "1.0.0";
+			String fileName = deploymentName;
+			// Remover la extension, si existe
+			int extensionIndex = fileName.lastIndexOf('.');
+			if (extensionIndex >= 0) {
+				fileName = fileName.substring(0,extensionIndex);
+			}
+			
+			// Remover el modulo y componente, si existe
+			if (fileName.contains(".")){
+			    String majorVersion = fileName.substring(0, fileName.indexOf("."));
+			    String minorVersion = fileName.substring(fileName.indexOf("."));
+			    int delimiter = majorVersion.lastIndexOf("-");
+			    if (majorVersion.indexOf("_")>delimiter) 
+			    	delimiter = majorVersion.indexOf("_");
+			    majorVersion = majorVersion.substring(delimiter+1, fileName.indexOf("."));
+			    version = majorVersion + minorVersion;
+			}			
+		}catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Exception occured while parsing version from deployment " + deploymentName + ". " + e.getMessage());
+			version = "1.0.0";
+		}
+		
+		try{
+			name = deploymentName;
+			name = name.replace(extension, "");
+			name = name.replace(version, "");
+			if(name.endsWith("-")){
+				name = name.substring(0,name.length() - 1);
+			}
+			
+			if (name.contains("-")){
+				int hyphenIndex = name.indexOf('-');				
+				if (hyphenIndex >= 0) {
+					String part1 = name.substring(hyphenIndex+1);
+					String part2 = name.substring(0,hyphenIndex);
+					
+					if(part1.length() > part2.length()){
+						name = part1;
+						module = part2;
+					}else{
+						module = part1;
+						name = part2;
+					}
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Exception occured while parsing name from deployment " + deploymentName + ". " + e.getMessage());
+			module = deploymentName;
+			name = deploymentName;
+		}
+		
+		entity.setAssetType("Application");
+		entity.setName(name);
+		entity.setVersion(version);
+		entity.setDescription("Aplicacion de " + module + " cargada con Harvester, de tipo " +  extension);
+		
+		
+		entity.addRelationship(server, "deployment", false);
+		
+		return entity;
+	}
+	
+	
+	private ModelControllerClient createClient(final InetAddress host, final int port, final String username, final String password, final String securityRealmName) {
+		final CallbackHandler callbackHandler = new CallbackHandler() {
+			public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+				for (Callback current : callbacks) {
+					if (current instanceof NameCallback) {
+						NameCallback ncb = (NameCallback) current;
+						logger.debug("JBoss username = " + new String(username));
+						ncb.setName(new String(username));
+					} else if (current instanceof PasswordCallback) {
+						PasswordCallback pcb = (PasswordCallback) current;
+						logger.debug("JBoss Password = " + password);
+						pcb.setPassword(password.toCharArray());
+					} else if (current instanceof RealmCallback) {
+						RealmCallback rcb = (RealmCallback) current;
+						logger.debug("JBoss Realm = " + rcb.getDefaultText());
+						rcb.setText(securityRealmName);
+					} else {
+						throw new UnsupportedCallbackException(current);
+					}
+				}
+			}
+		};
+		return ModelControllerClient.Factory.create(host, port, callbackHandler);
+	}
+	
 }
